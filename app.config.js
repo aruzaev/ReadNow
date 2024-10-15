05e953315a6a69e9bf63ce4d894b4393a8b65dd7
aruzaev <ruzaev.artem21@gmail.com>
2024-10-15 18:57:15 -0400

    Added google authentication

---
+++ app.config.js
@@ -0,0 +1,36 @@
+export default {
+  expo: {
+    name: "pock",
+    slug: "pock",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.aruzaev.pock",
+      googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.aruzaev.pock",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "b58cf0fa-d640-460d-82eb-472e62688036",
+      },
+    },
+  },
+};
